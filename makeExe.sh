43ed01e9afd20fe99c7428417f3af216a487f3f1
gsilvi <giorgio.silvi@quantumsimulations.de>
2022-09-01 16:29:00 +0200

    updated env with pyinstaller

--- makeExe.sh
+++ makeExe.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 conda activate PaperOracle
-# pyinstaller --hidden-import="pkg_resources.py2_warn" --hidden-import="googleapiclient" --hidden-import="apiclient"  --copy-metadata tqdm --copy-metadata regex --copy-metadata requests --copy-metadata packaging --copy-metadata filelock --copy-metadata numpy --copy-metadata tokenizers  --hidden-import=“sklearn.utils._cython_blas” --hidden-import=“sklearn.neighbors.typedefs” --hidden-import=“sklearn.neighbors.quad_tree” --hidden-import=“sklearn.tree” --hidden-import=“sklearn.tree._utils” UI.py
 pyinstaller  --copy-metadata tqdm  --copy-metadata regex --copy-metadata requests --copy-metadata packaging --copy-metadata filelock --copy-metadata numpy --copy-metadata tokenizers UI.py
